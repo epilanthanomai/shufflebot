fba2f2c9cec51148fb1606d58f94444ee64b946e
Hope Ranker <hope.ranker@gmail.com>
2020-03-20 17:32:33 -0400

    subclass Client, generalize command logic

--- shufflebot.py
+++ shufflebot.py
@@ -1,21 +1,48 @@
+import logging
 import os
-import discord
 
-client = discord.Client()
+from discord import Client
 
-
-@client.event
-async def on_ready():
-    print("Logged in as {0.user}".format(client))
+logger = logging.getLogger("shufflebot")
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+class BotBase(Client):
+    DEFAULT_COMMAND_PREFIX = "+"
 
-    if message.content.startswith("$hello"):
+    def __init__(self, *args, **kwargs):
+        self.command_prefix = kwargs.pop("command_prefix", self.DEFAULT_COMMAND_PREFIX)
+        super().__init__(*args, **kwargs)
+
+    async def on_ready(self):
+        logger.info(f"Logged in as {self.user}")
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if not message.content.startswith(self.command_prefix):
+            return
+
+        unprefixed = message.content[len(self.command_prefix) :]
+        command_name, _, rest = (s.strip() for s in unprefixed.strip().partition(" "))
+        command = getattr(self, "command_" + command_name, self.unrecognized_command)
+        try:
+            logger.debug(f"Command {command_name} from {message.author}")
+            await command(message, rest)
+        except:
+            logger.exception("Error while handling command")
+
+    async def unrecognized_command(self, message, rest):
+        pass
+
+
+class ShuffleBot(BotBase):
+    async def command_hello(self, message, rest):
         await message.channel.send("Hello!")
 
 
-client.run(os.environ["DISCORD_TOKEN"])
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    logger.setLevel(logging.DEBUG)
+    bot = ShuffleBot()
+    bot.run(os.environ["DISCORD_TOKEN"])
