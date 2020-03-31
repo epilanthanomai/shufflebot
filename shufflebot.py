4e9844b5fc6a8adb581912b4da3308cbd71fe03b
Hope Ranker <hope.ranker@gmail.com>
2020-03-30 22:59:22 -0400

    tease out a basic Card abstraction

--- shufflebot.py
+++ shufflebot.py
@@ -1,3 +1,4 @@
+import collections
 import logging
 import os
 import random
@@ -6,16 +7,16 @@ import discobot
 
 logger = logging.getLogger("shufflebot")
 
+Card = collections.namedtuple("Card", ["name", "description"])
+
 
 class CardStack:
-    RANKS = ("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
-    SUITS = "♣♦♥♠"
-
-    def __init__(self):
+    def __init__(self, cards):
+        self.card_set = cards
         self.reset()
 
     def reset(self):
-        self.cards = [rank + suit for suit in self.SUITS for rank in self.RANKS]
+        self.cards = self.card_set.copy()
 
     def shuffle(self):
         random.shuffle(self.cards)
@@ -27,6 +28,15 @@ class CardStack:
         return card
 
 
+POKER_RANKS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+POKER_SUITS = "♣♦♥♠"
+POKER_CARDS = [
+    Card(name=rank + suit, description=None)
+    for suit in POKER_SUITS
+    for rank in POKER_RANKS
+]
+
+
 class ShuffleBot(discobot.BotBase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -35,14 +45,14 @@ class ShuffleBot(discobot.BotBase):
     def get_cards(self, channel, create=True):
         result = self.channels.get(channel.id)
         if result is None:
-            result = CardStack()
+            result = CardStack(POKER_CARDS)
             self.channels[channel.id] = result
         return result
 
     async def command_scandeck(self, message, rest):
         """Output the entire contents of the deck. It'll be spoilered in a channel."""
         cards = self.get_cards(message.channel)
-        cards_str = ", ".join(cards.cards)
+        cards_str = ", ".join(card.name for card in cards.cards)
         await message.channel.send(
             f"{len(cards.cards)} cards in the deck: "
             + discobot.maybe_spoiler(cards_str, message.channel)
@@ -50,7 +60,8 @@ class ShuffleBot(discobot.BotBase):
 
     async def command_reset(self, message, rest):
         """Reset the deck to its original, unshuffled state."""
-        self.channels[message.channel.id] = CardStack()
+        cards = self.get_cards(message.channel)
+        cards.reset()
         await message.add_reaction("👍")
 
     async def command_shuffle(self, message, rest):
@@ -66,11 +77,19 @@ class ShuffleBot(discobot.BotBase):
         if not card:
             await message.channel.send("There are no cards left in the deck.")
         elif discobot.single_peer(message.channel):
-            await message.channel.send(card)
+            await message.channel.send(format_card_message(card))
         else:
             await message.channel.send(
-                f"{message.author.mention} drew {discobot.spoiler(card)}"
+                f"{message.author.mention} drew "
+                f"{discobot.spoiler(format_card_message(card))}"
             )
+
+
+def format_card_message(card):
+    if card.description is not None:
+        return f'"{card.name}"\n' + discobot.quote_all(card.description)
+    else:
+        return card.name
 
 
 if __name__ == "__main__":
